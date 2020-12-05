31466870b84292f491a831d5b97a09478f7ea7db
Erick Teixeira <erickhenriquemota@gmail.com>
2020-12-04 23:52:17 -0300

    Adiciona pulo básico

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
+    [SerializeField] public float runSpeed = 20f;
+    [SerializeField] private float movementSmoothing = .1f;
     private float horizontalMove;
     private Rigidbody2D rb;
-
-    [SerializeField] public float runSpeed = 5f;
-    [SerializeField] private float movementSmoothing = .1f;
     private Vector3 m_Velocity = Vector3.zero;
 
     [Header("Grounded")]
@@ -19,8 +18,8 @@ public class PlayerMovement : MonoBehaviour
     private float groundedRadius = .2f;
 
     [Header("Jump")]
-    [SerializeField] private float jumpForce = 400f;
-    public float fallJumpMultiplier = 2.5f;
+    [SerializeField] private float jumpForce = 300;
+    public float fallJumpMultiplier = 5f;
 
     void Start()
     {
@@ -31,16 +30,57 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         horizontalMove = Input.GetAxis("Horizontal") * runSpeed;
+        Debug.Log(isGrounded);
+
+        bool jump = false;
+        if (Input.GetKeyDown(KeyCode.Space))
+                jump = true;
+
+        Move(jump);
+        jump = false;
     }
 
     private void FixedUpdate() {
-        Move();
+        bool wasGrounded = isGrounded;
+        isGrounded = false;
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundedRadius, whatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                isGrounded = true;
+                if (!wasGrounded) {
+                    Debug.Log("Tocou o chão!");
+                    //Criar partículas de poeira
+                }
+            }
+        }
     }
 
-    public void Move()
+    public void Move(bool jump)
     {
-        Vector3 targetVelocity = new Vector2(horizontalMove * 10f, rb.velocity.y);
+        Vector3 targetVelocity = new Vector2(horizontalMove * Time.fixedDeltaTime * 10f, rb.velocity.y);
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref m_Velocity,movementSmoothing);
+
+        HandleJump(jump);
+        HandleFallJumpMultiplier(jump);
+    }
+
+    private void HandleJump(bool jump)
+    {
+        if(isGrounded && jump) {
+            isGrounded = false;
+            rb.AddForce(new Vector2(0f, jumpForce));
+        }
+    }
+
+    private void HandleFallJumpMultiplier(bool jump) 
+    {
+        if (rb.velocity.y < 0)
+            rb.velocity += Time.deltaTime * Physics2D.gravity.y * (fallJumpMultiplier - 1) * Vector2.up;
+        else if(rb.velocity.y > 0 && !jump) 
+            rb.velocity += Time.deltaTime * Physics2D.gravity.y * (fallJumpMultiplier - 1) * Vector2.up;
     }
 
 }
